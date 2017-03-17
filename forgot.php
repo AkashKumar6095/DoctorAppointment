d997f92fedcf6015fc93bd204a6c4d5d401a5c51
AkashKumar6095 <sara.kumar9195@gmail.com>
2017-03-17 22:07:56 +0530

    Add files via upload

---
+++ forgot.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title><link rel="stylesheet" type="text/css" href="css/forgot.css">
+</head>
+<body>
+	<div class="mainWrapp">
+		<div class="midWraps">
+			<div class="wrapper3">
+				<div class="details">
+					<form action="" method="" accept-charset="utf-8">
+						<ul class="formtypetabs">
+							<li class="inputtype">
+								<label>Enter Registered E-mail-ID/Mobile No.</label>
+								<input type="text" name="" value="" required="required" placeholder="E-mail-ID/Mobile No.">
+							</li>
+							<li class="submitbc">
+								<a href="#" title="">< BACK</a>
+								<input type="submit" name="" value="Reset Password">
+							</li>
+						</ul>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
