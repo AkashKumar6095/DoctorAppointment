afa87af52f90f6dbc3c576a3df57c1dc39054cc1
AkashKumar6095 <sara.kumar9195@gmail.com>
2017-03-19 16:45:40 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,5 +2,5 @@
 $connection=mysqli_connect("localhost", "root", "", "medicity");  //create connection 
 
 
-$siteroot="http://127.0.0.1/Doctor2";  //Get Root URL
+$siteroot="http://127.0.0.1/Doctor2";  //Declare Website URL
 ?>
