f7626f89dbd92f406abe599f2bc987165db93a02
AkashKumar6095 <sara.kumar9195@gmail.com>
2017-03-20 22:55:43 +0530

    Update about.php

--- about.php
+++ about.php
@@ -1,48 +1,78 @@
-<?php include('header.php');?>          //includes Header
-<link rel="stylesheet" type="text/css" href="css/about.css">    
+<!--About Us-->
+<?php include('header.php');?>          
+<link rel="stylesheet" type="text/css" href="css/about.css">  
+<!--main start-->
 <div class="main">
+	        <!--Header Image-->
 		<div class="headimg">
 				<div class="imgbox">
 					<img src="images/doctor.png" alt="">
 				</div>
 			</div>
+	        <!--mid wrap start-->
 		<div class="midWrap">
+			<!--wrapper start-->
 			<div class="wrapper">
+				<!--top wrap start-->
 				<div class="topwrap">
+					<!--Heading-->
 					<h1>About Us<span></span></h1>
 				</div>
+				<!--body wrap end-->
+				<!--marquee start-->
 				<div class="marquee">
+					<--Heading-->
 					<h2>Your Doctor is just a click away!!!</h2>
 				</div>
+				<!--marquee start-->
+				<!--body wrap start-->
 				<div class="bodyWrap">
+					<!--para start-->
 					<div class="para">
+						<!--Paragraph-->
 						<p>
 							Making a doctor appointment is an important feature of the complete medical website which provides all the possible medical assistance/services without any inconvenience or hassles and also saves a lot of time, money and more importantly provides unmatched medical help to people instantly.<br><br>
 							Doctor 	Appointment Booking System also provides the complete list of the doctors that are available at each location along with their experience in the medical field so that the patient is assured that the quality of treatment is never compromised and he/she gets the best medical treatment easily and conveniently.<br><br>
 						</p>
 					</div>
+					<!--para end-->
+					<!--headings start-->
 					<div class="headings">
+						<!--Heading-->
 						<h2>Our Mission</h2>
 					</div>
+					<!--headings end-->
+					<!--para start-->
 					<div class="para">
+						<!--Paragraph-->
 						<p>
 							To provide best and quality medical assistance to people at lowest prices and immediately.
 						</p>
 					</div>
-
+                                        <!--para end-->
+					<!--headings start-->
 					<div class="headings">
+						<!--Heading-->
 						<h2>Our Vision</h2>
 					</div>
+					<!--headings end-->
+					<!--para start-->
 					<div class="para">
+						<!--Paragraph-->
 						<p>
 							The Doctor appointment booking system is designed to help the person make appointments in just a click of a button anytime-anywhere<br> (as per the location) without waiting for the doctor.<br><br>
 						</p>
 					</div>
-
+                                        <!--para end-->
+					<!--headings start-->
 					<div class="headings">
+						<!--Heading-->
 						<h2>Features</h2>
 					</div>
+					 <!--headings end-->
+					<!--para start-->
 					<div class="para">
+						<!--Paragraphs-->
 						<p>
 							<span></span>Easy, Hassle-free and reliable way to book an appointment with the nearest doctor.<br><br>
 							<span></span>No need to wait for tokens or standing in long queues.<br><br>
@@ -54,21 +84,27 @@
 
 						</p>
 					</div>
-
-
-
+                                        <!--para end-->
+                                        <!--headings start-->
 					<div class="headings">
+						<!--Heading-->
 						<h2></h2>
 					</div>
+					<!--headings end-->
+					<!--para start-->
 					<div class="para">
+						<!--Paragraph-->
 						<p>
 							For further assistance/questions please feel free to<a href="http://127.0.0.1/Doctor2/contact.php" title=""> contact us.</a>
 						</p>
 					</div>
-
+                                        <!--para end-->
 			</div>
+		        <!--body wrap end-->
 		</div>
+	        <!--wrapper end-->
 	</div>
+	 <!--mid wrap end-->
 	</div>
-
-	<?php include('footer.php');?>      //includes Footer
+        <!--main end-->
+	<?php include('footer.php');?>  
