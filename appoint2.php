176730a5c017d1bbb217884e582d2a901947ea96
AkashKumar6095 <sara.kumar9195@gmail.com>
2017-04-02 17:48:31 +0530

    Update appoint2.php

--- appoint2.php
+++ appoint2.php
@@ -2,34 +2,34 @@
 <?php
 if(isset($_GET['did']))
 {
-	$did=$_GET['did'];
+	$did=$_GET['did'];        //assign value of variable
 
 
-	$sql="SELECT * FROM `doctors` WHERE `d_id` = '$did'";
-								$res=mysqli_query($connection, $sql);
-								$row=mysqli_fetch_assoc($res);
+	$sql="SELECT * FROM `doctors` WHERE `d_id` = '$did'";     //fetch all doctors and dentists from doctors table where value of d_id column are equal to did variable
+	$res=mysqli_query($connection, $sql);                      //perform queries $connection is connection and &sql is the resultmode
+	$row=mysqli_fetch_assoc($res);                            // Associative array
 								
 									
-									$catid=$row['cat_id'];
-									$doctor_name=$row['d_name'];
-									$d_type=$row['d_type'];
-									$d_spec=$row['d_spec'];
-									$d_qual=$row['d_qual'];
-									$d_clname=$row['d_clname'];
-									$d_claddr=$row['d_claddr'];
+	$catid=$row['cat_id'];
+	$doctor_name=$row['d_name'];
+	$d_type=$row['d_type'];
+	$d_spec=$row['d_spec'];
+        $d_qual=$row['d_qual'];
+	$d_clname=$row['d_clname'];
+	$d_claddr=$row['d_claddr'];
 
-									$available_in_morn_from=$row['available_in_morn_from'];
-									$available_in_morn_to=$row['available_in_morn_to'];
-									$available_in_eve_from=$row['available_in_eve_from'];
-									$available_in_eve_to=$row['available_in_eve_to'];
-									$appoi_time_minuts=$row['appoi_time_minuts'];
+	$available_in_morn_from=$row['available_in_morn_from'];
+	$available_in_morn_to=$row['available_in_morn_to'];
+	$available_in_eve_from=$row['available_in_eve_from'];
+	$available_in_eve_to=$row['available_in_eve_to'];
+	$appoi_time_minuts=$row['appoi_time_minuts'];
 
-									$sqlcat="SELECT * FROM `doctor_category` WHERE `cat_id` = '$catid'";
-									$rescat=mysqli_query($connection, $sqlcat);
-									$rowcat=mysqli_fetch_assoc($rescat);
-									$cat_name=$rowcat['cat_name'];
+	$sqlcat="SELECT * FROM `doctor_category` WHERE `cat_id` = '$catid'";  //fetch all doctors and dentists categories from doctor_category table where values in cat_id column are equal to catid variable
+        $rescat=mysqli_query($connection, $sqlcat);                           //perform queries $connection is connection and &sqlcat is the resultmode
+	$rowcat=mysqli_fetch_assoc($rescat);                                  // Associative array
+	$cat_name=$rowcat['cat_name'];
 
-									$today_date=date('Y-m-d');
+        $today_date=date('Y-m-d');
 
 
 				
@@ -39,6 +39,7 @@ if(isset($_GET['did']))
 	exit();
 }
 ?>
+         <!-- Custom CSS -->  
 	<link rel="stylesheet" type="text/css" href="css/appoint2.css">
 	<link rel="stylesheet" type="text/css" href="css/mobile.css">
         <!--main start-->
