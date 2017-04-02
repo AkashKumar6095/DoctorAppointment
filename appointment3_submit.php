c5419f93c11ed51a14590771ea18deb4f991edc0
AkashKumar6095 <sara.kumar9195@gmail.com>
2017-04-02 20:37:07 +0530

    Update appointment3_submit.php

--- appointment3_submit.php
+++ appointment3_submit.php
@@ -39,8 +39,7 @@ $sql="UPDATE `slot_availability` SET `status` = 'booked' WHERE `slot_id` = '$sid
 $res=mysqli_query($connection, $sql);
 
 ?>
-
-
+<!-------------------------------------------- Styling the page starts ---------------------------------------------------------->
 <div style="width: 400px; position: absolute; left: 0; right: 0; margin: auto; background-color: #279e29; color:#fff; padding: 10px;">
 <h3 style="float: left; width: 100%; text-align: center;">Congratulations!</h3>
 <p style="float: left; width: 100%; text-align: center;">Your Appointment has been booked.</p>
@@ -56,6 +55,7 @@ $res=mysqli_query($connection, $sql);
 <h3 style="float: left; width: 100%; text-align: center;">Sorry!</h3>
 <p style="float: left; width: 100%; text-align: center;">This Slot is already booked.</p>
 <div style="float: left; width: 100%">
+<!--Continue Button Takes Back to the Home Page after booking the apppointment-->	
 <a href="<?php echo $siteroot;?>" style="margin: auto; display: block; width: 100px; text-align: center; background-color: #fff; color:#333; line-height: 35px; height: 35px; text-decoration: none;">Continue</a>
 </div>
 </div>
