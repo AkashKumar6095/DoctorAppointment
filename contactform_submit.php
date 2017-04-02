14da43417eee9287fcb6d826bd18141354b9dd7f
AkashKumar6095 <sara.kumar9195@gmail.com>
2017-04-02 21:20:53 +0530

    Update contactform_submit.php

--- contactform_submit.php
+++ contactform_submit.php
@@ -23,7 +23,7 @@ $sql="INSERT INTO `contact_us` (`name`, `email`, `phone`, `city`, `type`, `messa
 mysqli_query($connection, $sql); //executes queries
 
 ?>	
-<!--Styling the Page starts-->
+<!-------------------------------------------- Styling the page starts ---------------------------------------------------------->
 <div style="width: 400px; position: absolute; left: 0; right: 0; margin: auto; background-color: #279e29; color:#fff; padding: 10px;">
 <h3 style="float: left; width: 100%; text-align: center;">Thank You For Contacting Us!</h3>
 <p style="float: left; width: 100%; text-align: center;">We will get back to you shortly.</p>
@@ -31,4 +31,4 @@ mysqli_query($connection, $sql); //executes queries
 <a href="<?php echo $siteroot;?>/contact.php" style="margin: auto; display: block; width: 100px; text-align: center; background-color: #fff; color:#333; line-height: 35px; height: 35px; text-decoration: none;">Continue</a>
 </div>
 </div>
-<!--Styling the page ends-->
+<!-------------------------------------------- Styling the page ends ---------------------------------------------------------->
