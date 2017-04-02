a3a5eda3590d4cf4f6f68e7572183cc245cba5e8
AkashKumar6095 <sara.kumar9195@gmail.com>
2017-04-02 19:00:36 +0530

    Update appointment3.php

--- appointment3.php
+++ appointment3.php
@@ -2,51 +2,57 @@
 <?php
 if(isset($_GET['sid']))
 {
-	$sid=$_GET['sid'];
+	$sid=$_GET['sid'];           //assign value of variable
 
-								$sql="SELECT * FROM `slot_availability` WHERE `slot_id` = '$sid'";
-								$res=mysqli_query($connection, $sql);
-								$row=mysqli_fetch_assoc($res);
-								$did=$row['doctor_id'];
+	$sql="SELECT * FROM `slot_availability` WHERE `slot_id` = '$sid'";   //fetch all slots from slot_availability table where value of slot_id column are equal to s_id variable
+        $res=mysqli_query($connection, $sql);          //perform queries $connection is connection and &sql is the resultmode
+	$row=mysqli_fetch_assoc($res);                 // Associative array
+        $did=$row['doctor_id']; 
 
 
-								$sql="SELECT * FROM `doctors` WHERE `d_id` = '$did'";
-								$res=mysqli_query($connection, $sql);
-								$row=mysqli_fetch_assoc($res);
+	$sql="SELECT * FROM `doctors` WHERE `d_id` = '$did'";    //fetch all doctors and dentists from doctors table where value of d_id column are equal to did variable
+	$res=mysqli_query($connection, $sql);                    //perform queries $connection is connection and &sql is the resultmode
+	$row=mysqli_fetch_assoc($res);                           // Associative array
 								
 									
-									$catid=$row['cat_id'];
-									$doctor_name=$row['d_name'];
-									$d_type=$row['d_type'];
-									$d_spec=$row['d_spec'];
-									$d_qual=$row['d_qual'];
-									$d_clname=$row['d_clname'];
-									$d_claddr=$row['d_claddr'];
+	$catid=$row['cat_id'];
+	$doctor_name=$row['d_name'];
+	$d_type=$row['d_type'];
+        $d_spec=$row['d_spec'];
+	$d_qual=$row['d_qual'];
+	$d_clname=$row['d_clname'];
+	$d_claddr=$row['d_claddr'];
 
-									$available_in_morn_from=$row['available_in_morn_from'];
-									$available_in_morn_to=$row['available_in_morn_to'];
-									$available_in_eve_from=$row['available_in_eve_from'];
-									$available_in_eve_to=$row['available_in_eve_to'];
-									$appoi_time_minuts=$row['appoi_time_minuts'];
+	$available_in_morn_from=$row['available_in_morn_from'];
+	$available_in_morn_to=$row['available_in_morn_to'];
+	$available_in_eve_from=$row['available_in_eve_from'];
+	$available_in_eve_to=$row['available_in_eve_to'];
+	$appoi_time_minuts=$row['appoi_time_minuts'];
 
-									$sqlcat="SELECT * FROM `doctor_category` WHERE `cat_id` = '$catid'";
-									$rescat=mysqli_query($connection, $sqlcat);
-									$rowcat=mysqli_fetch_assoc($rescat);
-									$cat_name=$rowcat['cat_name'];
+	$sqlcat="SELECT * FROM `doctor_category` WHERE `cat_id` = '$catid'";   //fetch all doctors and dentists doctor_category from doctors table where value of catid_id column are equal to catid variable
+	$rescat=mysqli_query($connection, $sqlcat);                            //perform queries $connection is connection and &sqlcat is the resultmode
+	$rowcat=mysqli_fetch_assoc($rescat);                                    // Associative array
+        $cat_name=$rowcat['cat_name'];
 }else
 {
 	exit();
 }
 ?>
+       <!-- Custom CSS-->
 	<link rel="stylesheet" type="text/css" href="css/appointment.css">
 	<link rel="stylesheet" type="text/css" href="css/mobile.css">
+        <!--main start-->
 	<div class="main">
+		 <!--midWrap start-->
 		<div class="midWrap">
+			 <!--bodyWrap start-->
 			<div class="bodyWrap">
+				<!--Doctor Image-->
 				<div class="profile">
 					<div class="profilepic">
 						<img src="images/Male-placeholder.jpg" alt="">
 					</div>
+					<!--details start-->
 					<div class="details">
 						<h2><?php echo $doctor_name;?></h2>
 						<div class="leftdetail">
@@ -56,14 +62,25 @@ if(isset($_GET['sid']))
 							<p>Qualification</p>
 						</div>
 						<div class="leftdetail2">
-							<p>: <?php echo $d_clname;?></p>
-							<p>: <?php echo $d_claddr;?></p>
-							<p>: <?php echo $cat_name;?></p>
-							<p>: <?php echo $d_qual;?></p>
+							<p>: <?php echo $d_clname      //display clinic name from the database
+	                                                    ;?>
+							</p>
+							<p>: <?php echo $d_claddr     //display clinic address from the database
+	                                                     ;?>
+							</p>
+							<p>: <?php echo $cat_name      //display category name from the database
+	                                                     ;?>
+							</p>
+							<p>: <?php echo $d_qual       //display doctor qualification from the database
+	                                                     ;?>
+							</p>
 						</div>
 					</div>
+					<!--details end-->
 				</div>
+				<!--Wrapper class start-->
 				<div class="Wrapper">
+			        <!--Form Starts and after it is submitted it is sent for processing to appointment3_submit.php file with the HTTP Post Method-->
 				<form action="appointment3_submit.php" method="post" accept-charset="utf-8">
 					<input type="hidden" name="sid" value="<?php echo $sid;?>">
 					<div class="formBox">
@@ -71,13 +88,14 @@ if(isset($_GET['sid']))
 						<label>Patient Name<span>*</span>
 						<input type="text" name="patname_name" value="" required="required"></label>
 						<label for="age">Patient Age<span>*</span>
+					        <!--Form Validation for Patient Age-->
 						<input type="text" name="patage_age" required="required" pattern="^\d{1,3}$" max="110"></label>
 						<label>Type of Problem<span>*</span>
 							<select name="category" id="category">
 							<?php
-							$sql="SELECT * FROM `doctor_category`";
-							$res=mysqli_query($connection, $sql);
-							while($row=mysqli_fetch_assoc($res))
+							$sql="SELECT * FROM `doctor_category`";   //fetch all categories from doctor_category table
+							$res=mysqli_query($connection, $sql);     //perform queries $connection is connection and &sql is the resultmode
+							while($row=mysqli_fetch_assoc($res))    
 							{
 								$cat_id=$row['cat_id'];
 								$cat_name=$row['cat_name'];
@@ -99,28 +117,36 @@ if(isset($_GET['sid']))
 						<input type="text" name="explain_submit" value=""></label>
 						<label>Email<span>*</span>
 						<input type="email" name="email_submit" value="" required="required"></label>
+						<!--Form Validation For Contact No-->
 						<label>Contact No.<span>*</span>
 						<input type="text" name="contact_submit" pattern="[789][0-9]{9}" value="" required="required"></label>
 						<label>Patient Address
 						<input type="text" name="pataddress_submit" value=""></label>
 						<ul class="submit">
 							<li class="red">
+								<!--Cancel Button Redirects to previous page-->
 								<a href="<?php echo $_SERVER['HTTP_REFERER']?>" title="">Cancel</a>
 							</li>
 							<li class="grey">
+								<!--Refreshes the page-->
 								<input type="reset" name="" value="Refresh Form">
 							</li>
 							<li class="green">
+								<!--Submit Button-->
 								<input type="submit" name="Submit" value="Set An Appointment">
 							</li>
 						</ul>
 					</div>
 				</form>
+			        <!--Form ends-->		
 				</div>
+				<!--wrapper class end-->
 			</div>
+			<!--bodyWrap end-->
 		</div>
+		<!--midWrap end-->
 	</div>
-
+        <!--main end-->
 <script type="text/javascript">
 	$("#category").change(function(){
 		if($(this).val()=="others")
