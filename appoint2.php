011b5115867fd875f7e27051f3af69f31b8224f3
AkashKumar6095 <sara.kumar9195@gmail.com>
2017-04-29 13:37:18 +0530

    Update appoint2.php

--- appoint2.php
+++ appoint2.php
@@ -10,24 +10,24 @@ if(isset($_GET['did']))           //until the variable set of did is not null
 	$row=mysqli_fetch_assoc($res);                            // Associative array
 								
 									
-	$catid=$row['cat_id'];
-	$doctor_name=$row['d_name'];
-	$d_type=$row['d_type'];
-	$d_spec=$row['d_spec'];
-        $d_qual=$row['d_qual'];
-	$d_clname=$row['d_clname'];
-	$d_claddr=$row['d_claddr'];
+	$catid=$row['cat_id'];                      //assign value of cat_id stored in the database to catid variable row wise
+	$doctor_name=$row['d_name'];                //assign value of d_name stored in the database to doctor_name variable row wise
+	$d_type=$row['d_type'];                     //assign value of d_type stored in the database to d_type variable row wise
+	$d_spec=$row['d_spec'];                     //assign value of d_spec stored in the database to d_spec variable row wise
+        $d_qual=$row['d_qual'];                     //assign value of d_qual stored in the database to d_qual variable row wise
+	$d_clname=$row['d_clname'];                 //assign value of cl_name stored in the database to cl_name variable row wise
+	$d_claddr=$row['d_claddr'];                 //assign value of d_claddr stored in the database to d_claddr variable row wise
 
-	$available_in_morn_from=$row['available_in_morn_from'];
-	$available_in_morn_to=$row['available_in_morn_to'];
-	$available_in_eve_from=$row['available_in_eve_from'];
-	$available_in_eve_to=$row['available_in_eve_to'];
-	$appoi_time_minuts=$row['appoi_time_minuts'];
+	$available_in_morn_from=$row['available_in_morn_from'];  //assign value of available_in_morn_from stored in the database to available_in_morn_from variable row wise
+	$available_in_morn_to=$row['available_in_morn_to'];      //assign value of available_in_morn_to stored in the database to available_in_morn_to variable row wise
+	$available_in_eve_from=$row['available_in_eve_from'];    //assign value of available_in_eve_from stored in the database to available_in_eve_from variable row wise
+	$available_in_eve_to=$row['available_in_eve_to'];        //assign value of available_in_eve_to stored in the database to available_in_eve_to variable row wise
+	$appoi_time_minuts=$row['appoi_time_minuts'];            //assign value of appoi_time_minuts stored in the database to appoi_time_minuts variable row wise
 
 	$sqlcat="SELECT * FROM `doctor_category` WHERE `cat_id` = '$catid'";  //fetch all doctors and dentists categories from doctor_category table where values in cat_id column are equal to catid variable
         $rescat=mysqli_query($connection, $sqlcat);                           //perform queries $connection is connection and &sqlcat is the resultmode
 	$rowcat=mysqli_fetch_assoc($rescat);                                  // Associative array
-	$cat_name=$rowcat['cat_name'];
+	$cat_name=$rowcat['cat_name'];                      
 
         $today_date=date('Y-m-d');          //assign the date to today_date variable in the Y-M-D Format
 
